df54c3882e06a1fcc38724a96012559b49af84ca
Deependra Singh <deependra.is.dev@gmail.com>
2024-01-10 15:36:21 +0530

    Add install dependecy in circleci config

--- app/test_x.py
+++ app/test_x.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
-from .admin import some_admin_app
+from .admin import some_admin_app, some_more_fun
 class YourTestCase(TestCase):
     def test_example(self):
         # Your test logic here
         some_admin_app()
+        some_more_fun()
         self.assertEqual(1 + 1, 2)
